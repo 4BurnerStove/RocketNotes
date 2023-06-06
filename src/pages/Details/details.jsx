e7b9dab30f7be525ec8a1109f8f5607ec304fb38
Andre Morryson <amorrysson77@gmail.com>
2023-06-06 16:16:53 -0300

    Alterated componentes

---
+++ src/pages/Details/details.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react"
+import './styles.css'
+
+
+
+export function Details() {
+
+  return (
+    <Fragment>
+      <h1>Hello, world!</h1>,
+      <span>Andre morryson </span>
+    </Fragment>
+  )
+}
