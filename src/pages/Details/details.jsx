e9dd2d84a08c20482e86fcac83fe4b0e517887d0
Andre Morryson <amorrysson77@gmail.com>
2023-06-08 14:52:12 -0300

    Create a button component

--- src/pages/Details/details.jsx
+++ src/pages/Details/details.jsx
@@ -1,13 +1,18 @@
 import { Container } from './styles'
-
+import { Button } from '../../components/Button'
 
 
 export function Details() {
 
   return (
     <Container>
-      <h1>Hello, world!</h1>,
+      <h1>Hello, world!</h1>
       <span>Andre morryson </span>
+
+
+      <Button name='Login'></Button>
+      <Button name='Cadastrar' ></Button>
+      <Button name='Sair' ></Button>
     </Container>
   )
 }
