097e53ec42983e66de11e61a7a763e81cd85a770
Andre Morryson <amorrysson77@gmail.com>
2023-06-12 19:15:39 -0300

    initial home base page

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
-import { Details } from './pages/Details/details'
 import theme from './styles/theme'
+
+// import { Details } from './pages/Details/details'
+import { Home } from './pages/Home'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
