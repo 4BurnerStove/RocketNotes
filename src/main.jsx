97a4e5297eb2f8dec7af3d3294276f79ffc8f49b
Andre Morryson <amorrysson77@gmail.com>
2023-06-20 15:34:07 -0300

    Create a componente TextArea and create a page New

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,15 @@ import theme from './styles/theme'
 // import { Home } from './pages/Home'
 // import { SignIn } from './pages/SignIn'
 // import { SignUp } from './pages/SignUp'
-import { Profile } from './pages/Profile'
+// import { Profile } from './pages/Profile'
+import { NewNote } from './pages/NewNote'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Profile />
+      <NewNote />
     </ThemeProvider>
   </React.StrictMode>,
 )
