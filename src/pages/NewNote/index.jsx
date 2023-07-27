94dba6f7c547127d2723457e4602867cfd30d598
Andre Morryson <amorrysson77@gmail.com>
2023-07-27 11:16:26 -0300

    create a newNote function

--- src/pages/NewNote/index.jsx
+++ src/pages/NewNote/index.jsx
@@ -7,16 +7,23 @@ import { NoteItem } from '../../components/NoteItem'
 import { Section } from '../../components/Section'
 import { Button } from '../../components/Button'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+
+import { api } from '../../services/api'
 
 import { Container, Form } from './styles'
 
 export function NewNote() {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
   const [links, setLinks] = useState([])
   const [newLink, setNewLink] = useState('')
 
+  const [tags, setTags] = useState([])
+  const [newTags, setNewTags] = useState('')
 
-  const [tag, setTag] = useState([])
-  const [newTag, setNewTag] = useState('')
+  const navigate = useNavigate()
 
   function handleAddLink() {
     if (!newLink) {
@@ -31,15 +38,40 @@ export function NewNote() {
   }
 
   function handleAddTag() {
-    if (!newTag) {
+    if (!newTags) {
       return
     }
-    setTag(prevState => [...prevState, newTag])
-    setNewTag('')
+
+
+    setTags(prevState => [...prevState, newTags])
+    setNewTags('')
   }
 
-  function handleRemoveTag(delet) {
-    setTag(prevState => prevState.filter(tag => tag !== delet))
+  function handleRemoveTags(delet) {
+    setTags(prevState => prevState.filter(tags => tags !== delet))
+  }
+
+  async function handleNewNote() {
+    if(!title){
+      return alert('Digite o título da nota')
+    }
+
+    if (newTags) {
+      return alert("Você esqueceu de adicionar um marcador, clique ou remova o conteúdo")
+    }
+    if (newLink) {
+      return alert("Você esqueceu de adicionar um link, adicione ou remova o conteúdo")
+    }
+
+    await api.post('/notes', {
+      title,
+      description,
+      tags,
+      links
+    })
+
+    alert("Nota criada com sucesso!")
+    navigate('/')
   }
 
   return (
@@ -51,8 +83,14 @@ export function NewNote() {
             <h1>Criar nota</h1>
             <Link to="/">Voltar</Link>
           </header>
-          <Input placeholder='Título' />
-          <Textarea placeholder='Observações'></Textarea>
+          <Input
+            placeholder='Título'
+            onChange={e => setTitle(e.target.value)}
+          />
+          <Textarea
+            placeholder='Observações'
+            onChange={e => setDescription(e.target.value)}
+          ></Textarea>
 
           <Section name='Links úteis'>
             {
@@ -78,27 +116,26 @@ export function NewNote() {
           <Section name='Marcadores'>
             <div className='tags'>
               {
-                tag.map((tag, index) => (
+                tags.map((tags, index) => (
                   <NoteItem
                     key={String(index)}
-                    value={tag}
-                    onClick={() => handleRemoveTag(tag)}
+                    value={tags}
+                    onClick={() => handleRemoveTags(tags)}
                   />
                 ))
               }
 
-
               <NoteItem
                 isNew
                 placeholder='Novo Marcador'
-                onChange={e => setNewTag(e.target.value)}
-                value={newTag}
+                onChange={e => setNewTags(e.target.value)}
+                value={newTags}
                 onClick={handleAddTag}
               ></NoteItem>
             </div>
 
           </Section>
-          <Button name='Salvar'></Button>
+          <Button name='Salvar' onClick={handleNewNote}></Button>
         </Form>
       </main>
 
